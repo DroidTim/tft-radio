8846e38d0ccb56996300e33b0eef5e00a1277a78
Tobias2212 <tobiasknauss@wk-vertrieb.eu>
2024-09-15 20:19:18 +0200

    implements Error Screen

--- app/src/main/java/de/iu/tftradio/presentation/ErrorScreen.tk.kt
+++ app/src/main/java/de/iu/tftradio/presentation/ErrorScreen.tk.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -15,9 +16,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import de.iu.tftradio.R
+import java.io.IOException
 
 @Composable
-fun ErrorScreen() {
+fun ErrorScreen(exception: Throwable) {
+    val errorMessage: String
+    val errorImage: Painter
+
+    when (exception) {
+        is IOException -> {
+            errorImage = painterResource(id = R.drawable.ic_error)
+            errorMessage = stringResource(id = R.string.check_network_message)
+        }
+        else -> {
+            errorImage = painterResource(id = R.drawable.error)
+            errorMessage = stringResource(id = R.string.try_again_message)
+        }
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
@@ -32,12 +48,12 @@ fun ErrorScreen() {
         ) {
             // Icon
             Image(
-                painter = painterResource(id = R.drawable.ic_error),
+                painter = errorImage,
                 contentDescription = stringResource(id = R.string.error_Image),
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
-                    .size(100.dp)
-                    .padding(bottom = 16.dp)
+                    .size(150.dp)
+
             )
 
             // Large text
@@ -47,16 +63,31 @@ fun ErrorScreen() {
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
                 color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.padding(bottom = 8.dp)
+                modifier = Modifier.padding(bottom = 10.dp) .padding(top = 60.dp)
             )
 
             // Smaller text
             Text(
-                text = stringResource(id = R.string.check_network_message),
+                text = errorMessage,
                 fontSize = 14.sp,
                 color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 100.dp)
             )
+            when (exception.localizedMessage) {
+                is String -> {
+                    Text(
+                        text = exception.localizedMessage as String,
+                        fontSize = 10.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f),
+                        textAlign = TextAlign.Center
+                    )
+
+                }
+                else -> {
+
+                }
+            }
         }
     }
 }
@@ -64,5 +95,5 @@ fun ErrorScreen() {
 @Preview
 @Composable
 fun PreviewErrorScreen() {
-    ErrorScreen()
+    ErrorScreen(exception = IOException("Ein Fehler ist aufgetreten, tut uns Leid"))
 }
